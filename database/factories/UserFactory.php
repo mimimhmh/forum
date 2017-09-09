20114c2bb3d22e3f84c9dae159b318468996716e
Yang Wang <mimimhmh@users.noreply.github.com>
2017-09-09 15:57:27 +1200

    migration seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,12 +14,44 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\User::class, function (Faker $faker) {
+
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Reply::class, function (Faker $faker) {
+
+    return [
+        'user_id'   => function () {
+
+            return factory('App\User')->create()->id;
+        },
+        'thread_id' => function () {
+
+            return factory('App\Thread')->create()->id;
+        },
+        'body'      => $faker->paragraph()
+    ];
+
+});
+
+$factory->define(App\Thread::class, function (Faker $faker) {
+
+    return [
+        'user_id' => function () {
+
+            return factory('App\User')->create()->id;
+        },
+        'title'   => $faker->sentence(),
+        'body'    => $faker->paragraph()
+    ];
+
+});
+
+
