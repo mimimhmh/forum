3756f98943d18e1d42fa72f99e2910101234bc66
Yang Wang <mimimhmh@users.noreply.github.com>
2017-09-11 11:09:37 +1200

    Resource routing

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/threads', 'ThreadsController@index');
-
-Route::get('/threads/{thread}', 'ThreadsController@show');
-
-Route::post('/threads', 'ThreadsController@store');
+Route::resource('/threads', 'ThreadsController');
 
 Route::post('/threads/{thread}/replies', 'RepliesController@store');
