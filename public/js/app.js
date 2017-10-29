7454af5bd892bb6f779c934e9698566a252d23bc
Yang Wang <mimimhmh@users.noreply.github.com>
2017-10-30 01:22:18 +1300

    Username Wrapping with anchor

--- public/js/app.js
+++ public/js/app.js
@@ -60309,7 +60309,7 @@ var render = function() {
                 ]
               )
             ])
-          : _c("div", { domProps: { textContent: _vm._s(_vm.body) } })
+          : _c("div", { domProps: { innerHTML: _vm._s(_vm.body) } })
       ]),
       _vm._v(" "),
       _c(
